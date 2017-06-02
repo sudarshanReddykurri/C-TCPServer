fa1361029139ac0ac6da60d94a99de1a08e1e994
SudarshanReddy kurri <kurrisudarshanaug12@hotmail.com>
2017-06-02 19:53:41 +0530

    Newly Added Files

---
+++ TCPServer/TCPServer/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbConsoleOutput = new System.Windows.Forms.TextBox();
+            this.tbIPAddress = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.btnStartListening = new System.Windows.Forms.Button();
+            this.lbl_ipaddress = new System.Windows.Forms.Label();
+            this.lbl_Port = new System.Windows.Forms.Label();
+            this.tbPayload = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFindIP = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbClients = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tbConsoleOutput
+            // 
+            this.tbConsoleOutput.Location = new System.Drawing.Point(21, 12);
+            this.tbConsoleOutput.Multiline = true;
+            this.tbConsoleOutput.Name = "tbConsoleOutput";
+            this.tbConsoleOutput.Size = new System.Drawing.Size(649, 314);
+            this.tbConsoleOutput.TabIndex = 0;
+            this.tbConsoleOutput.TextChanged += new System.EventHandler(this.tbConsoleOutput_TextChanged);
+            // 
+            // tbIPAddress
+            // 
+            this.tbIPAddress.Location = new System.Drawing.Point(57, 396);
+            this.tbIPAddress.Name = "tbIPAddress";
+            this.tbIPAddress.Size = new System.Drawing.Size(178, 20);
+            this.tbIPAddress.TabIndex = 1;
+            this.tbIPAddress.Text = "127.0.0.1";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(300, 396);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 2;
+            this.tbPort.Text = "23000";
+            // 
+            // btnStartListening
+            // 
+            this.btnStartListening.Location = new System.Drawing.Point(57, 446);
+            this.btnStartListening.Name = "btnStartListening";
+            this.btnStartListening.Size = new System.Drawing.Size(178, 23);
+            this.btnStartListening.TabIndex = 3;
+            this.btnStartListening.Text = "Start Listening";
+            this.btnStartListening.UseVisualStyleBackColor = true;
+            this.btnStartListening.Click += new System.EventHandler(this.btnStartListening_Click);
+            // 
+            // lbl_ipaddress
+            // 
+            this.lbl_ipaddress.AutoSize = true;
+            this.lbl_ipaddress.Location = new System.Drawing.Point(34, 399);
+            this.lbl_ipaddress.Name = "lbl_ipaddress";
+            this.lbl_ipaddress.Size = new System.Drawing.Size(17, 13);
+            this.lbl_ipaddress.TabIndex = 4;
+            this.lbl_ipaddress.Text = "IP";
+            this.lbl_ipaddress.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_Port
+            // 
+            this.lbl_Port.AutoSize = true;
+            this.lbl_Port.Location = new System.Drawing.Point(252, 400);
+            this.lbl_Port.Name = "lbl_Port";
+            this.lbl_Port.Size = new System.Drawing.Size(26, 13);
+            this.lbl_Port.TabIndex = 5;
+            this.lbl_Port.Text = "Port";
+            // 
+            // tbPayload
+            // 
+            this.tbPayload.Location = new System.Drawing.Point(528, 397);
+            this.tbPayload.Name = "tbPayload";
+            this.tbPayload.Size = new System.Drawing.Size(155, 20);
+            this.tbPayload.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(528, 446);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(155, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(466, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Payload";
+            // 
+            // btnFindIP
+            // 
+            this.btnFindIP.Location = new System.Drawing.Point(300, 446);
+            this.btnFindIP.Name = "btnFindIP";
+            this.btnFindIP.Size = new System.Drawing.Size(100, 23);
+            this.btnFindIP.TabIndex = 9;
+            this.btnFindIP.Text = "Find IP";
+            this.btnFindIP.UseVisualStyleBackColor = true;
+            this.btnFindIP.Click += new System.EventHandler(this.btnFindIP_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(677, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Clients:";
+            // 
+            // lbClients
+            // 
+            this.lbClients.FormattingEnabled = true;
+            this.lbClients.Location = new System.Drawing.Point(676, 36);
+            this.lbClients.Name = "lbClients";
+            this.lbClients.Size = new System.Drawing.Size(120, 290);
+            this.lbClients.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 510);
+            this.Controls.Add(this.lbClients);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFindIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbPayload);
+            this.Controls.Add(this.lbl_Port);
+            this.Controls.Add(this.lbl_ipaddress);
+            this.Controls.Add(this.btnStartListening);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbIPAddress);
+            this.Controls.Add(this.tbConsoleOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConsoleOutput;
+        private System.Windows.Forms.TextBox tbIPAddress;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Button btnStartListening;
+        private System.Windows.Forms.Label lbl_ipaddress;
+        private System.Windows.Forms.Label lbl_Port;
+        private System.Windows.Forms.TextBox tbPayload;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFindIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbClients;
+    }
+}
+
---
+++ TCPServer/TCPServer/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace TCPServer
+{
+    public partial class Form1 : Form
+    {
+
+        TcpListener mTCPListener;
+
+        TcpClient mTCPClient;
+
+        byte[] mRx;
+
+        //Newly Added 1 line
+
+        private List<ClientNode> mlClientSocks;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Newly Added 2lines 
+            mlClientSocks = new List<ClientNode>(2);
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        IPAddress findMyIPV4Address()
+        {
+            string strThisHostName = string.Empty;
+            IPHostEntry thisHostDNSEntry = null;
+            IPAddress[] allIPsOfThisHost = null;
+            IPAddress ipv4Ret = null;
+            try
+            {
+                strThisHostName = System.Net.Dns.GetHostName();
+
+                printLine(strThisHostName);
+
+                thisHostDNSEntry = System.Net.Dns.GetHostEntry(strThisHostName);
+
+                allIPsOfThisHost = thisHostDNSEntry.AddressList;
+
+                for(int idx = allIPsOfThisHost.Length-1; idx >0; idx--)
+                {
+                    if (allIPsOfThisHost[idx].AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ipv4Ret = allIPsOfThisHost[idx];
+                    }
+                }
+
+
+            }            
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return ipv4Ret;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartListening_Click(object sender, EventArgs e)
+        {
+
+
+            IPAddress ipaddr;
+            int nPort;
+
+            if (!int.TryParse(tbPort.Text, out nPort))
+            {
+                nPort = 23000;
+            }
+
+            if(!IPAddress.TryParse(tbIPAddress.Text, out ipaddr))
+            {
+                MessageBox.Show("Invalid IP address supplied");
+                return;
+            }
+
+            mTCPListener = new TcpListener(ipaddr,nPort);
+
+            mTCPListener.Start();
+
+            mTCPListener.BeginAcceptTcpClient(onCompleteAcceptTcpClient, mTCPListener);
+
+
+
+
+        }
+
+        void onCompleteAcceptTcpClient(IAsyncResult iar)
+        {
+
+            TcpListener tcpl = (TcpListener)iar.AsyncState;
+
+            //Newly Added 2 lines 
+            TcpClient tclient = null;
+            ClientNode cNode = null;
+
+            try
+            {
+                // //Newly Added 1 line
+                tclient = tcpl.EndAcceptTcpClient(iar);
+
+                
+                // mTCPClient = tcpl.EndAcceptTcpClient(iar);
+                printLine("Client Connected....");
+
+                tcpl.BeginAcceptTcpClient(onCompleteAcceptTcpClient, tcpl);
+                // mRx = new byte[512];
+                //mTCPClient.GetStream().BeginRead(mRx, 0, mRx.Length, onCompletedReadFromTCPClientStream, mTCPClient);
+
+
+                //Newly Added 4 lines excluding flower brackets
+                lock (mlClientSocks)
+                {
+                    mlClientSocks.Add((cNode = new ClientNode(tclient, new byte[512], new byte[512], tclient.Client.RemoteEndPoint.ToString())));
+                    lbClients.Items.Add(cNode.ToString());
+                    
+                }
+
+                tclient.GetStream().BeginRead(cNode.Rx, 0, cNode.Rx.Length, onCompleteReadFromTCPClientStream, tclient);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+         }
+
+        void onCompleteReadFromTCPClientStream(IAsyncResult iar)
+        {
+
+            //TcpClient tcpc;
+            //int nCountReadBytes = 0;
+            //string strRecv;
+
+            //try
+            //{
+            //    tcpc = (TcpClient)iar.AsyncState;
+            //    nCountReadBytes = tcpc.GetStream().EndRead(iar);
+
+            //    if (nCountReadBytes == 0)
+            //    {
+            //        MessageBox.Show("Client disconnected.");
+            //        return;
+            //    }
+
+            //    strRecv = Encoding.ASCII.GetString(mRx, 0, nCountReadBytes);
+
+            //    printLine(strRecv);
+
+            //    mRx = new byte[512];
+
+            //    tcpc.GetStream().BeginRead(mRx, 0, mRx.Length, onCompleteReadFromTCPClientStream, tcpc);
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+
+
+
+
+            // Newly Added Below Lines of this method
+
+            TcpClient tcpc;
+            int nCountReadBytes = 0;
+            string strRecv;
+            
+            ClientNode cn = null;
+
+            try
+            {
+
+
+                lock (mlClientSocks)
+                {
+                tcpc = (TcpClient)iar.AsyncState;
+                cn = mlClientSocks.Find(x => x.strId == tcpc.Client.RemoteEndPoint.ToString());
+
+                    nCountReadBytes = tcpc.GetStream().EndRead(iar);
+
+                if(nCountReadBytes==0) // this happens when the client is disconnected
+                 {
+                    MessageBox.Show("Client Disconnected");
+                        mlClientSocks.Remove(cn);
+                        lbClients.Items.Remove(cn.ToString());
+                        return;
+                 }
+
+                    // strRecv = Encoding.ASCII.GetString(mRx, 0, nCountReadBytes);
+
+
+                    //     printLine(strRecv);
+
+                    //  mRx = new byte[512];
+
+                    // tcpc.GetStream().BeginRead(mRx, 0, mRx.Length, onCompleteReadFromTCPClientStream, tcpc);
+
+
+                    strRecv = Encoding.ASCII.GetString(cn.Rx, 0, nCountReadBytes).Trim();
+                    //strRecv = Encoding.ASCII.GetString(mRx, 0, nCountReadBytes);
+
+                    printLine(DateTime.Now + " - " + cn.ToString() + ": " + strRecv);
+
+                    cn.Rx = new byte[512];
+
+                    tcpc.GetStream().BeginRead(cn.Rx, 0, cn.Rx.Length, onCompleteReadFromTCPClientStream, tcpc);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lock (mlClientSocks)
+                {
+                    printLine("Client disconnected: " + cn.ToString());
+                    mlClientSocks.Remove(cn);
+                    lbClients.Items.Remove(cn.ToString());
+                }
+            }
+
+        }
+
+        public void printLine(string _strPrint)
+        {
+            tbConsoleOutput.Invoke(new Action<string>(doInvoke), _strPrint);
+        }
+        public void doInvoke(string _strPrint)
+        {
+            tbConsoleOutput.Text = _strPrint + Environment.NewLine + tbConsoleOutput.Text;
+        }
+
+        private void tbConsoleOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //byte[] tx = new byte[512];
+
+            //if (string.IsNullOrEmpty(tbPayload.Text))
+            //    return;
+            //try
+            //{
+            //    if(mTCPClient != null)
+            //    {
+            //        if (mTCPClient.Client.Connected)
+            //        {
+            //            tx = Encoding.ASCII.GetBytes(tbPayload.Text);
+            //            mTCPClient.GetStream().BeginWrite(tx,0,tx.Length, onCompleteWriteToClientStream, mTCPClient);
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+
+            // //Newly Added all the below lines of this method
+
+            if (lbClients.Items.Count <= 0) return;
+            if (string.IsNullOrEmpty(tbPayload.Text)) return;
+
+            ClientNode cn = null;
+
+            lock (mlClientSocks)
+            {
+                cn = mlClientSocks.Find(x => x.strId == lbClients.SelectedItem.ToString());
+                cn.Tx = new byte[512];
+
+                try
+                {
+                    if (cn != null)
+                    {
+                        if (cn.tclient != null)
+                        {
+                            if (cn.tclient.Client.Connected)
+                            {
+                                cn.Tx = Encoding.ASCII.GetBytes(tbPayload.Text);
+                                cn.tclient.GetStream().BeginWrite(cn.Tx, 0, cn.Tx.Length, onCompleteWriteToClientStream, cn.tclient);
+                            }
+                        }
+                    }
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void onCompleteWriteToClientStream(IAsyncResult iar)
+        {
+            try
+            {
+                TcpClient tcpc = (TcpClient)iar.AsyncState;
+                tcpc.GetStream().EndWrite(iar);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnFindIP_Click(object sender, EventArgs e)
+        {
+            IPAddress ipa = null;
+            ipa = findMyIPV4Address();
+            if (ipa != null)
+            {
+                tbIPAddress.Text = ipa.ToString();
+            }
+
+        }
+    }
+}
